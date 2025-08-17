6097a5cd97ce2141c6fbfdbffe4411de43cbb2dc
Jia-A <agrawaljiya2011@gmail.com>
2025-08-17 13:34:41 +0530

    feat: implement grouped anagram class with two methods and add to index

---
+++ DSA/groupedAnagram.js
@@ -0,0 +1,41 @@
+console.log("Grouped anagram")
+
+class GroupedAnagram{
+    groupedAnagram(arr){
+        const map = {}
+        for (let str of arr){
+            const sortedStr = str.split("").sort().join("");
+            if(!map[sortedStr]){
+                map[sortedStr] = [];
+            }
+            map[sortedStr].push(str);
+        }
+        return Object.values(map);
+    }
+
+    groupedAnagram2(arr){
+
+        const map = {};
+        for(let str of arr){
+                    
+            const charArr = new Array(26).fill(0)
+            for (let char of str){
+                const idx = char.charCodeAt(0) - 'a'.charCodeAt(0)
+                charArr[idx]++;
+            }
+            console.log(map);
+            const keyString = charArr.join(",");
+            if(!map[keyString]){
+                map[keyString] = [];
+            }
+            map[keyString].push(str)  
+        }
+        return Object.values(map)
+    }
+}
+
+const groupedAnagram = new GroupedAnagram();
+console.log(groupedAnagram.groupedAnagram(["eat", "tea", "tan", "ate", "nat", "bat"])); 
+console.log(groupedAnagram.groupedAnagram2(["eat", "tea", "tan", "ate", "nat", "bat"])); 
+
+console.log("--------------------------");
--- DSA/twoSum.js
+++ DSA/twoSum.js
@@ -1,3 +1,36 @@
 console.log("Two sum problem")
 
+class TwoSum{
+
+    twoSumIndices(arr, target){
+        const newMap = {};
+        for(let i = 0; i < arr.length; i++){ // { 2 : 0, 3 : 1, 11 : 2, 15 : 3, 7: 4 }
+            newMap[arr[i]] = i
+        }
+
+        for(let i = 0; i < arr.length; i++){
+            const diff = target - arr[i] // 9 - 2 = 7
+            if(newMap[diff] !== undefined && newMap[diff] !== i) // key present or not in the object newMap["7"] && value of newMap["7"] !== 0
+            return [i, newMap[diff]]
+        }
+        return []
+    }
+
+    twoSumIndices2(arr, target){
+        const newMap = new Map();
+        for(let i=0; i<arr.length; i++){
+            const diff = target - arr[i];
+            if(newMap.has(diff) && newMap.get(diff) !== i){
+                return [newMap.get(diff), i];
+            }
+            newMap.set(arr[i], i); // set the value of arr[i] as key
+        }
+        return []
+    }
+}
+
+const twoSum = new TwoSum();
+console.log(twoSum.twoSumIndices([2, 3, 11, 15, 7], 9))
+console.log(twoSum.twoSumIndices2([2, 3, 11, 15, 7], 9))
+
 console.log("--------------------------");
